407360633b7c1af1f3e2a5c70848b56bdd4c3fdb
DesnLee <jiakun.ui@gmail.com>
2023-03-20 17:03:02 +0800

    feat: welcome 页面添加过渡动画

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,10 +1,32 @@
-import type { FC } from 'react';
-import { Outlet } from 'react-router-dom';
+import { animated, useTransition } from '@react-spring/web';
+import { useRef } from 'react';
+import type { FC, ReactNode } from 'react';
+import { useLocation, useOutlet } from 'react-router-dom';
 
 export const WelcomeLayout: FC = () => {
-  return (
-    <div>
-      <Outlet />
-    </div>
-  );
+  const { pathname } = useLocation();
+  const cacheMap = useRef<Record<string, ReactNode>>({});
+  cacheMap.current[pathname] = useOutlet();
+
+  const transitions = useTransition(pathname, {
+    from: {
+      opacity: pathname === '/welcome/1' ? 1 : 0,
+      transform: `translate3d(${pathname === '/welcome/1' ? 0 : 100}%, 0, 0px)`,
+    },
+    enter: {
+      opacity: 1,
+      transform: 'translate3d(0%, 0, 0px)',
+    },
+    leave: {
+      opacity: 0,
+      transform: 'translate3d(-50%, 0, -40px)',
+    },
+    config: { duration: 300 },
+  });
+
+  return transitions((style, i) => (
+    <animated.div style={style} key={i}>
+      {cacheMap.current[i]}
+    </animated.div>
+  ));
 };
