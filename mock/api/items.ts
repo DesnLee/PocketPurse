13795ec8231a8cd6c974d7362098c19b052f80fc
DesnLee <jiakun.ui@gmail.com>
2023-03-22 19:05:51 +0800

    chore: 为 mock 接口增加 timeout

--- mock/api/items.ts
+++ mock/api/items.ts
@@ -5,6 +5,7 @@ export const itemsAPI: MockMethod = {
   url: '/api/v1/items',
   method: 'get',
   statusCode: 200,
+  timeout: 1500,
   response: () => {
     return itemsData.succeed;
   },
--- mock/api/user.ts
+++ mock/api/user.ts
@@ -5,6 +5,7 @@ export const userAPI: MockMethod = {
   url: '/api/v1/user',
   method: 'get',
   statusCode: 200,
+  timeout: 1500,
   response: () => {
     return userData.succeed;
   },
