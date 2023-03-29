dfafe4fded8620bb0e748f89e877bd3ed89eddd4
DesnLee <jiakun.ui@gmail.com>
2023-03-29 19:52:23 +0800

    refactor: 重新封装 TopNav

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,14 +1,31 @@
-import type { FC } from 'react';
-import { useMenuStore } from '../stores/useMenuStore';
+import type { FC, ReactNode } from 'react';
 import { Icon } from './Icon';
 
-export const TopNav: FC = () => {
-  const { isVisible, setVisible } = useMenuStore();
+interface Props {
+  title?: string;
+  leftIcon?: ReactNode;
+  color?: string;
+}
+export const TopNav: FC<Props> = ({
+  title = 'PocketPurse',
+  leftIcon,
+  color,
+}) => {
   return (
-    <div flex items-center py-16px color-primary>
-      <Icon name='menu' size='24px' onClick={() => setVisible(!isVisible)} />
-      <h1 ml-16px text-18px font-bold>
-        PocketPurse
+    <div flex items-center py-16px color={color ?? '[var(--color-primary)]'}>
+      <span
+        w-24px
+        h-24px
+        flex
+        justify-center
+        items-center
+        children-w-24px
+        children-h-24px
+      >
+        {leftIcon || <Icon name='arrow_back' size='24px' />}
+      </span>
+      <h1 ml-8px text-18px font-bold>
+        {title}
       </h1>
     </div>
   );
--- src/views/Items/Items.tsx
+++ src/views/Items/Items.tsx
@@ -2,24 +2,30 @@ import { useState } from 'react';
 import type { FC } from 'react';
 import {
   AddFloatBtn,
+  Icon,
   TimeRangePicker,
   TopMenu,
   TopNav,
 } from '../../components';
 import type { TimeRange } from '../../components/TimeRangePicker';
 import { useTitle } from '../../hooks';
+import { useMenuStore } from '../../stores/useMenuStore';
 import { ItemList } from './ItemList';
 import { ItemsSummary } from './ItemsSummary';
 
 export const Items: FC = () => {
-  useTitle('项目');
+  useTitle('账单列表');
 
   const [currentRange, setCurrentRange] = useState<TimeRange>('thisMonth');
+  const { isVisible, setVisible } = useMenuStore();
 
   return (
     <div flex flex-col h-full>
       <header gradient-topnav shadow-primary px-16px shrink-0>
-        <TopNav />
+        <TopNav
+          title='账单列表'
+          leftIcon={<Icon name='menu' onClick={() => setVisible(!isVisible)} />}
+        />
         <TimeRangePicker current={currentRange} onChange={setCurrentRange} />
       </header>
       <main grow-1 overflow-scroll>
