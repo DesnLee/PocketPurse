71112aa1d7f5c33ce9c696686bbeeb126cb0a5ea
DesnLee <jiakun.ui@gmail.com>
2023-04-01 15:27:08 +0800

    feat: 完善 Tags 列表样式

--- src/views/newItem/Tags.tsx
+++ src/views/newItem/Tags.tsx
@@ -1,4 +1,22 @@
 import type { FC } from 'react';
+import styled from 'styled-components';
+import { Icon } from '../../components';
+
+const IconWrapper = styled.div<{ selected?: boolean }>`
+  width: 56px;
+  height: 56px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #0000000b;
+  border-radius: 28px;
+  font-size: 24px;
+  text-align: center;
+  border-width: 2px;
+  border-style: solid;
+  border-color: ${({ selected }) =>
+    selected ? 'var(--color-primary)' : 'transparent'};
+`;
 
 export const Tags: FC = () => {
   const tags = Array.from<string>({ length: 30 }).fill('😄');
@@ -12,19 +30,14 @@ export const Tags: FC = () => {
       p-16px
       px-10px
     >
+      <li w-56px>
+        <IconWrapper>
+          <Icon size='20px' color='var(--color-primary)' name='add' />
+        </IconWrapper>
+      </li>
       {tags.map((tag, i) => (
         <li w-56px key={tag}>
-          <div
-            w-56px
-            h-56px
-            bg='#0000000b'
-            rounded-28px
-            text-16px
-            text-center
-            leading-56px
-          >
-            {tag}
-          </div>
+          <IconWrapper selected={false}>{tag}</IconWrapper>
           <p text-12px leading-12px mt-6px text-center color='#606266'>
             tag{i}
           </p>
